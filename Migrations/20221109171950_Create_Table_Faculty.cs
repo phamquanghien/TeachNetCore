b55747ad6b3ccc12327379360f688a14d8ef0d9a
phamquanghien <pqh.cntt@gmail.com>
2022-11-10 00:28:00 +0700

    Create_ForeignKey_Student_FacultyID

---
+++ Migrations/20221109171950_Create_Table_Faculty.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableFaculty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FacultyID",
+                table: "Student",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Faculty",
+                columns: table => new
+                {
+                    FacultyID = table.Column<string>(type: "TEXT", nullable: false),
+                    FacultyName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faculty", x => x.FacultyID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Student_FacultyID",
+                table: "Student",
+                column: "FacultyID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Student_Faculty_FacultyID",
+                table: "Student",
+                column: "FacultyID",
+                principalTable: "Faculty",
+                principalColumn: "FacultyID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Student_Faculty_FacultyID",
+                table: "Student");
+
+            migrationBuilder.DropTable(
+                name: "Faculty");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Student_FacultyID",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "FacultyID",
+                table: "Student");
+        }
+    }
+}
